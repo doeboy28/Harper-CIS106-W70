bfd2e9ccac9c61f73912a3ed19b29c34b76f1185
doeboy28 <lk82215@mail.harpercollege.edu>
2026-09-10 15:28:48 -0500

    Update problem6.py

--- problem6.py
+++ problem6.py
@@ -1,11 +1,8 @@
 purchase_price = float(input("Enter purchase price per share: $"))
 current_price = float(input("Enter current stock price: $"))
 quantity = float(input("Enter quantity of stock: "))
-
 value_change = (current_price - purchase_price) * quantity
-
 print(f"Increase/decrease in stock value: ${value_change:.2f}")
-
 if value_change < 0:
     print("You are losing money.")
 elif value_change > 0:
