40c0ab5c24f1e64a3ccf349385bd04eb0155485b
doeboy28 <lk82215@mail.harpercollege.edu>
2026-09-10 15:29:56 -0500

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,8 +1,6 @@
 ticker = input("Enter stock ticker symbol: ").upper()
 shares = float(input("Enter number of shares: "))
 cost_per_share = float(input("Enter cost per share: $"))
-
 amount_invested = shares * cost_per_share
-
 print(f"Stock: {ticker}")
 print(f"Amount invested: ${amount_invested:.2f}")
